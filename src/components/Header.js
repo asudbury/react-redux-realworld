854f41b32ce2e232073bb87e403033c8a7de2565
Adrian Sudbury <sudburya@gmail.com>
2021-06-15 07:18:22 +0100

    Content Update.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const LoggedOutView = props => {
+const LoggedOutView = (props) => {
   if (!props.currentUser) {
     return (
       <ul className="nav navbar-nav pull-xs-right">
-
         <li className="nav-item">
           <Link to="/" className="nav-link">
             Home
           </Link>
         </li>
-
         <li className="nav-item">
           <Link to="/login" className="nav-link">
             Sign in
           </Link>
         </li>
-
         <li className="nav-item">
           <Link to="/register" className="nav-link">
             Sign up
           </Link>
         </li>
-
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Adrian Sudbury
+          </Link>
+        </li>
       </ul>
     );
   }
   return null;
 };
 
-const LoggedInView = props => {
+const LoggedInView = (props) => {
   if (props.currentUser) {
     return (
       <ul className="nav navbar-nav pull-xs-right">
-
         <li className="nav-item">
           <Link to="/" className="nav-link">
             Home
@@ -54,14 +54,15 @@ const LoggedInView = props => {
         </li>
 
         <li className="nav-item">
-          <Link
-            to={`/@${props.currentUser.username}`}
-            className="nav-link">
-            <img src={props.currentUser.image} className="user-pic" alt={props.currentUser.username} />
+          <Link to={`/@${props.currentUser.username}`} className="nav-link">
+            <img
+              src={props.currentUser.image}
+              className="user-pic"
+              alt={props.currentUser.username}
+            />
             {props.currentUser.username}
           </Link>
         </li>
-
       </ul>
     );
   }
@@ -74,7 +75,6 @@ class Header extends React.Component {
     return (
       <nav className="navbar navbar-light">
         <div className="container">
-
           <Link to="/" className="navbar-brand">
             {this.props.appName.toLowerCase()}
           </Link>
